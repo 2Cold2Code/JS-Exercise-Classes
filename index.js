4aeb773e850d05fab5ba3fdca0f02cc654d479c1
2Cold2Code <wizardofosburn@gmail.com>
2020-01-22 22:44:47 -0500

    finished Instructor class all tests passing

--- index.js
+++ index.js
@@ -136,8 +136,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian {
+    constructor(instructor) {
+        super(instructor)
+        this.specialty = instructor.specialty;
+        this.favLanguage = instructor.favLanguage;
+        this.catchPhrase = instructor.catchPhrase;
+    }
 
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
 }
 
 /*
